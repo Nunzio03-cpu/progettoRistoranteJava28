ff3eb399f67d813cc20a2011aaa5a1cf802ef3f6
BiagioG95 <biagiograssia95@gmail.com>
2024-11-13 18:05:33 +0100

    Delete src/Color.java
    cancellazione classe Color

--- src/Color.java
+++
@@ -1,7 +0,0 @@
-public class Color {
-    public static final String RESET = "\u001B[0m";
-    public static final String GIALLO = "\u001B[33m"; // utilizzo per nome del piatto
-    public static final String ROSSO = "\u001B[31m"; // utilizzo per prezzo
-    public static final String BLU = "\u001B[34m"; //utilizzo per ingredienti
-    public static final String VERDE = "\u001B[32m"; // field aggiuntivo in ogni classe figlia
-}
